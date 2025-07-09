0ea8531a6dc2a22c3bbc7c3a505f7cd513814ee9
manuelgulloso3 <gullosomanuel3@gmail.com>
2025-07-09 17:40:43 -0500

    ejercicio js

---
+++ contador.js
@@ -0,0 +1,11 @@
+let clics = 0;
+
+function hacerClics() {
+    clics = clics +1 ;
+    document.getElementById("contadorClics").innerHTML= clics;
+}
+
+function reiniciarClics() {
+    clics = 0;
+    document.getElementById("contadorClics").innerHTML = clics;
+}
---
+++ texto.js
@@ -0,0 +1,4 @@
+function cambiarTexto() {
+    const texto = document.getElementById("texto");
+    texto.innerText = "Técnico en Programación de Software";
+}
